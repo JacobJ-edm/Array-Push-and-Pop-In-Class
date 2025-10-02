b0b76e1e92bf555f474533fcc2be42112647c0cb
JacobJ-edm <204055029+JacobJ-edm@users.noreply.github.com>
2025-10-02 10:47:26 -0600

    finnite

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
 $(document).ready(function () {
-
+/*
+const unshiftArray = [];
+let unshiftnumber=0;
+let $unshift=$("#unshift-btn")
+$unshift.click(function(){
+unshiftArray.unshift(unshiftnumber);
+unshiftnumber++;
+console.log(unshiftArray);
+});
+let shiftnumber=0;
+let $shift=$("#shift-btn")
+$shift.click(function(){
+unshiftArray.shift(shiftnumber);
+shiftnumber++;
+console.log(unshiftArray);
+});
+const pushArray = [];
+let pushnumber=0;
+let $push=$("#push-btn")
+$push.click(function(){
+pushArray.push(pushnumber);
+pushnumber++;
+console.log(pushArray);
+});
+let popnumber=0;
+let $pop=$("#pop-btn")
+$pop.click(function(){
+pushArray.pop(popnumber);
+popnumber++;
+console.log(pushArray);
+});
+*/
+const newArray = [1,2,3];
+console.log(newArray)
+newArray[3] = 4;
+console.log(newArray);
+newArray.push(4);
+console.log(newArray);
 });
